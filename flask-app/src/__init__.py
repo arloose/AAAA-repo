9048bca0a318facd80ac1f181ad3362993d6f2e6
AayushT824 <67922789+AayushT824@users.noreply.github.com>
2022-12-13 23:05:22 -0500

    Add files via upload

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -16,22 +16,27 @@ def create_app():
 
     # these are for the DB object to be able to connect to MySQL. 
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123'
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'classicmodels'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'ABay' 
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
     
     # Import the various routes
     from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.products.products import products
+    from src.Users.Admins  import admins
+    from src.Users.Buyers  import buyers
+    from src.Users.Sellers  import sellers
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/classic')
-    app.register_blueprint(customers,   url_prefix='/classic')
-    app.register_blueprint(products,    url_prefix='/classic')
+    app.register_blueprint(sellers,   url_prefix='/sell')
+    app.register_blueprint(buyers,    url_prefix='/buy')
+    app.register_blueprint(admins,    url_prefix='/ad')
+    app.register_blueprint(products,    url_prefix='/prod')
+
 
     return app
