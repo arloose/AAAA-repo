da2b43ad5df45e418b5fc3d2eff112adfd456619
AayushT824 <67922789+AayushT824@users.noreply.github.com>
2022-12-13 23:05:03 -0500

    Add files via upload

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,6 +5,7 @@
 # import the create app function 
 # that lives in src/__init__.py
 from src import create_app
+from session import session
 
 # create the app object
 app = create_app()
@@ -14,4 +15,9 @@ if __name__ == '__main__':
     # this app will be bound to port 4000. 
     # Take a look at the docker-compose.yml to see 
     # what port this might be mapped to... 
+    session["buyerID"] = 1
+    session["sellerID"] = 7
+
+    #Since no login screen, hardcoded replacement values for session info
+    session["adminID"] = 1
     app.run(debug = True, host = '0.0.0.0', port = 4000)
